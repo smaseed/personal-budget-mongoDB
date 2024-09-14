7cddd8e88df592c7ce8e48e58e32f703a44d7fef
smaseed <smaseed@charlotte.edu>
2024-09-14 13:13:12 -0400

    Added server, Axios, and ChartJS

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+const budget = {
+    myBudget: [
+        {
+            title: 'Eat out',
+            budget: 25
+        },
+        {
+            title: 'Rent',
+            budget: 275
+        },
+        {
+            title: 'Grocery',
+            budget: 110
+        },
+    ]
+};
+
+app.get('/budget', (req, res) => {
+    res.json(budget)
+})
+
+app.listen(port, ()=> {
+    console.log("App served at :3000")
+})
