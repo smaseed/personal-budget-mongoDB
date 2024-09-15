cb227425c96bda611ce56300fd60b5af57a7f1e4
smaseed <smaseed@charlotte.edu>
2024-09-15 16:22:25 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const port = 3000;
 app.use('/', express.static('public'));
 
 const budget = require('./budget.json');
-// fetch("budget.json")
-//     .then(res => res.json())
-//     .then(data => {
-//         budget = data;
-//     })
-//     .catch(error => console.error("Error Loading JSON File:", error));
 
 app.get('/budget', (req, res) => {
     res.json(budget)
